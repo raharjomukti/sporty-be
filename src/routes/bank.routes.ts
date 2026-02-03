3be018b842ce270f4beb90a0b6e7ca7683340e7d
raharjomukti <raharjomukti09@gmail.com>
2026-02-03 23:21:50 +0700

    Live session 8

---
+++ src/routes/bank.routes.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import { authenticate } from "../middlewares/auth.middleware";
+import {
+    createBank, deleteBank, getBanks, updateBank,
+} from "../controllers/bank.controller";
+
+const router = Router();
+
+router.post("/", authenticate, createBank);
+router.get("/", getBanks);
+router.put("/", authenticate, updateBank);
+router.delete("/", authenticate, deleteBank);
+
+export default router;
